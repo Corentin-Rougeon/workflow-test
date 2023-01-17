e7592812691d0f45de7b5009bb9397edb9b6ff4a
Rougeon <corentin.rougeon@ynov.com>
2023-01-17 10:25:32 +0100

    workflow test (valid)

--- test_simplemath.py
+++ test_simplemath.py
@@ -3,5 +3,5 @@ from simplemath import SimpleMath
 
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
-        result = SimpleMath.addition(3, 2)
+        result = SimpleMath.addition(1, 2)
         self.assertEqual(result, 3)

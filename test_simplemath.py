75838c567d3df4431426abb4743b51d0e853d2b4
Rougeon <corentin.rougeon@ynov.com>
2023-01-17 10:40:47 +0100

    added substraction function

--- test_simplemath.py
+++ test_simplemath.py
@@ -5,3 +5,7 @@ class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(1, 2)
         self.assertEqual(result, 3)
+
+    def test_substraction(self):
+        result = SimpleMath.substraction(1, 2)
+        self.assertEqual(result,-1)
